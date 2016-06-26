302e13874fef2f1aaf63f5201258725fe4c043ae
Daniel Streicher <dstreicher@bitbucket.org>
2016-06-26 18:02:33 -0400

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+  var $scrollToTop = $('.scroll-to-top');
+  $scrollToTop.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $scrollToTop.fadeIn();
+    } else {
+      $scrollToTop.fadeOut();
+    }
+  });
+  $scrollToTop.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+
+  $(".navbar-nav .nav-link").click(function (event) {
+    var toggle = $(".navbar-toggle").is(":visible");
+    if (toggle) {
+      $("#navbar").collapse('hide');
+    }
+  });
+
+  $('nav').singlePageNav({
+    offset: $('.navbar').outerHeight(),
+    filter: ':not(.external)',
+    speed: 750,
+    currentClass: 'active'
+  });
+}); 
